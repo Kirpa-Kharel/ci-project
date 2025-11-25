dc7418fce544634496c6c94b4e254e9e7ba20f81
Kirpa-Kharel <kripakhrl7@gmail.com>
2025-11-25 10:42:56 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hi, my name is kirpa. \n');
+  res.end('Hi, my name is kirpa. speaker\n');
 });
 
 server.listen(PORT, () => {
